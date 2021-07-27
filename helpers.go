7010f202245d77dffd7b5ac0add3dbd65c755a3a
Emil Volckmar Ry <emil-volckmar.ry@norsk-tipping.no>
2021-07-27 08:30:17 +0200

    makeAbsolutePath

--- helpers.go
+++ helpers.go
@@ -1,11 +1,21 @@
 package main
 
-import "encoding/base64"
+import (
+	"encoding/base64"
+	"io/fs"
+	"path/filepath"
+)
 
 func isBase64(s string) bool {
-		_, err := base64.StdEncoding.DecodeString(s)
-		if err != nil {
-			return false
-		}
-		return true
+	_, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		return false
 	}
+	return true
+}
+
+func makeAbsolutePath(config config, filename fs.FileInfo) (newbase string) {
+	base := filepath.Join("declarative", config.destEnv, "sealedsecrets")
+	newbase = base + "/" + filename.Name()
+	return newbase
+}
