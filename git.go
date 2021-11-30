fbc6cee85d5863f8d11c3c2c66ac0bd169aa898f
Emil Volckmar Ry <emil-volckmar.ry@norsk-tipping.no>
2021-11-30 11:49:23 +0100

    monitoringmessage watches namelog before list is emptied

--- git.go
+++ git.go
@@ -123,8 +123,8 @@ func gitPush(config config) {
 		}
 		log.WithFields(log.Fields{"commitMessage": obj.Message, "When": obj.Committer.When, "action": "request.git.operation.pushed", "secret": secretNameLog}).Info("Raven updated files in git")
 		genericPostWebHook()
+		go monitorMessages(secretNameLog)
 		secretNameLog = []string{}
-		go monitorMessages(added)
 	}
 
 }
@@ -147,7 +147,6 @@ func initializeWorkTree(r *git.Repository) (w *git.Worktree) {
 
 func getGitStatus(worktree *git.Worktree) (status git.Status, err error) {
 	status, err = worktree.Status()
-
 	if err != nil {
 		log.WithFields(log.Fields{
 			"err": err,
